fe6233619249faf55a423275ce7ca0114b690e5a
Ahlam Abdulkhalek <alhamny@gmail.com>
2022-10-29 10:40:52 +0300

    Creating first models

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    age = models.IntegerField(default=1)
+
+class Song(models.Model):
+    artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length=40)
+    date = models.DateTimeField('Date released') 
+    likes = models.IntegerField(default=0)
+    artist_id = models.IntegerField(default=0)
+
+class Lyric(models.Model):
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.CharField(max_length=400)
